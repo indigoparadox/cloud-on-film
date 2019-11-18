9b8d0047775700796cc1a763a25fbed7909d63b9
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-11-18 17:58:53 -0500

    Added some common picture fields and migrate script.

--- src/cloud/models.py
+++ src/cloud/models.py
@@ -1,5 +1,12 @@
 
 from . import db
+from enum import Enum
+
+class HashEnum( Enum ):
+    md5 = 1
+    sha128 = 2
+    sha256 = 3
+
 
 class User( db.Model ):
 
@@ -21,10 +28,12 @@ class Tag( db.Model ):
 
     id = db.Column( db.Integer, primary_key=True )
     parent_id = db.Column( db.Integer, unique=False, nullable=True )
-    name = db.Column( db.String( 64 ), index=True, unique=False, nullable=False )
+    name = db.Column(
+        db.String( 64 ), index=True, unique=False, nullable=False )
     owner_id = db.Column( db.Integer, db.ForeignKey( 'users.id' ) )
     picture_id = db.Column( db.Integer, db.ForeignKey( 'pictures.id' ) )
-    pictures = db.relationship( 'Picture', secondary=pictures_tags, back_populates='tags' )
+    pictures = db.relationship(
+        'Picture', secondary=pictures_tags, back_populates='tags' )
 
 
 class Picture( db.Model ):
@@ -32,11 +41,26 @@ class Picture( db.Model ):
     __tablename__ = 'pictures'
 
     id = db.Column( db.Integer, primary_key=True )
-    relative_path = db.Column( db.String( 256 ), index=True, unique=True, nullable=True )
+    relative_path = db.Column(
+        db.String( 256 ), index=True, unique=True, nullable=True )
     library_id = db.Column( db.Integer, db.ForeignKey( 'libraries.id' ) )
     library = db.relationship( 'Library', back_populates='pictures' )
     tag_id = db.Column( db.Integer, db.ForeignKey( 'tags.id' ) )
-    tags = db.relationship( 'Tag', secondary=pictures_tags, back_populates='pictures' )
+    tags = db.relationship(
+        'Tag', secondary=pictures_tags, back_populates='pictures' )
+    timestamp = db.Column(
+        db.DateTime, index=False, unique=False, nullable=False )
+    filesize = db.Column(
+        db.Integer, index=False, unique=False, nullable=False )
+    width = db.Column( db.Integer, index=False, unique=False, nullable=False )
+    height = db.Column( db.Integer, index=False, unique=False, nullable=False )
+    added = db.Column( db.DateTime, index=False, unique=False, nullable=False )
+    filehash = db.Column(
+        db.String( 512 ), index=False, unique=False, nullable=False )
+    filehash_algo = db.Column(
+        db.Enum( HashEnum ), index=False, unique=False, nullable=False )
+    comment = db.Column( db.Text, index=False, unique=False, nullable=True )
+    nsfw = db.Column( db.Boolean, index=True, unique=False, nullable=False )
 
 
 class Library( db.Model ):
@@ -45,7 +69,11 @@ class Library( db.Model ):
 
     id = db.Column( db.Integer, primary_key=True )
     pictures = db.relationship( 'Picture', back_populates='library_id' )
-    display_name = db.Column( db.String( 64 ), index=False, unique=True, nullable=False )
-    machine_name = db.Column( db.String( 64 ), index=True, unique=True, nullable=False )
-    absolute_path = db.Column( db.String( 256 ), index=True, unique=True, nullable=False )
+    display_name = db.Column(
+        db.String( 64 ), index=False, unique=True, nullable=False )
+    machine_name = db.Column(
+        db.String( 64 ), index=True, unique=True, nullable=False )
+    absolute_path = db.Column(
+        db.String( 256 ), index=True, unique=True, nullable=False )
+
 
