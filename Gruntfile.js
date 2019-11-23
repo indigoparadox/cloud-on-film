ca4a56d029107c6f72be737550459463e0f1aadc
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-11-23 09:10:50 -0500

    Playing with lightbox.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,11 @@ module.exports = function( grunt ) {
                   'node_modules/bootstrap/dist/js/bootstrap.min.js',
                   'node_modules/bootstrap/dist/css/bootstrap.min.css',
                   'node_modules/popper.js/dist/umd/popper.min.js',
-                  "node_modules/unveil2/dist/jquery.unveil2.min.js"
+                  'node_modules/unveil2/dist/jquery.unveil2.min.js',
+                  'node_modules/featherlight/release/featherlight.min.css',
+                  'node_modules/featherlight/release/featherlight.min.js',
+                  'node_modules/featherlight/release/featherlight.gallery.min.css',
+                  'node_modules/featherlight/release/featherlight.gallery.min.js'
                ],
                dest: static_dir, flatten: true},
             ]
